1462d62724b1b5aa3fced03d873cde9ed1d77532
Maurice Pittet <maurice.pittet@lausanne.ch>
2018-03-22 10:05:38 +0100

    Use of expose-loader

--- src/components/myliste-plugin.js
+++ src/components/myliste-plugin.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import MyListe from './MyListe.vue'
-import 'expose-loader?$!expose-loader?jQuery!jquery'
+import jQuery from 'jquery'
 import 'semantic-ui/dist/semantic.css'
 import 'semantic-ui/dist/semantic.js'
 
